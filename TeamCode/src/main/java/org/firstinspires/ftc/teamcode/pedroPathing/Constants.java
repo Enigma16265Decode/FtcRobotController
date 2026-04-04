3f87deb2dd71ebc376ac258a6aef765c2b76917e
BudsterGaming <budstergaming@binaryflood.com>
2026-04-03 18:09:08 -0600

    ivy 0.0.1, plus pedro 2.1.1 tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -2,38 +2,31 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.pedropathing.control.FilteredPIDFCoefficients;
 import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.control.PredictiveBrakingCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
-
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(13) //old was tuned on 12.6 but true weight was 12.8
-            .forwardZeroPowerAcceleration(-34.491)
-            .lateralZeroPowerAcceleration(-68.440)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.2, 0, 0.03, 0.025)) //.translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0.025))
-            .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0.04, 0.025))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.08,0.0,0.0005,0.6,0.0))
-            .centripetalScaling(0.0015);
+            .mass(12.1)
+            .headingPIDFCoefficients(new PIDFCoefficients(0.7, 0, 0.025, 0.025))
+            //.translationalPIDFCoefficients(new PIDFCoefficients(0.16, 0, 0.015, 0.02))
+            //.drivePIDFCoefficients(new FilteredPIDFCoefficients(0.035,0.0,0.00002,0.6,0.02))
+            .predictiveBrakingCoefficients(new PredictiveBrakingCoefficients(0.2, 0.0809703292560898, 0.0018817585632387927))
+            .centripetalScaling(0)
+            .forwardZeroPowerAcceleration(-30.946573585390496)
+            .lateralZeroPowerAcceleration(-54.07392148817415);
 
-    public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(4.7)
-            .strafePodX(2.54)
-            .distanceUnit(DistanceUnit.INCH)
-            .hardwareMapName("pinpoint")
-            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -45,10 +38,17 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(78.315)
-            .yVelocity(60.738);
+            .xVelocity(85.921844242126)
+            .yVelocity(66.8676286832554);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-2.5)
+            .strafePodX(-5.25)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
